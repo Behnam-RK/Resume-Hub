38b0fb83f1b75c281b400b1accc1fb4c05941241
Behnam RK <Hyper.firstblood@gmail.com>
2018-07-14 15:33:18 +0430

    settings.py updated

--- ResumeHub/settings.py
+++ ResumeHub/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '6992ih634k@_tzx*-2*jwy009oljfxjgzpwoa+anjrx$wmy21)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'behnamrk.pythonanywhere.com'
+]
 
 
 # Application definition
